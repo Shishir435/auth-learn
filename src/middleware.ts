3cd344890e64f1b8972ed328d0006aaca0079252
Shishir435 <shishirchaurasiya435@gmail.com>
2024-03-22 23:49:38 +0530

    Refactor middleware.ts file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
-import {auth} from "@/../auth"
-export default auth((req)=>{
-    console.log(req)
+import { auth } from "@/../auth"
+export default auth((req) => {
+  console.log(req)
 })
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-  }
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
